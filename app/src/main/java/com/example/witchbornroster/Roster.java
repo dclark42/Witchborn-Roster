d07b65de85acf336e8ae688daf2b956492f9df28
dclark42 <davidjclark@lewisu.edu>
2019-05-02 21:51:19 -0500

    initial project

---
+++ app/src/main/java/com/example/witchbornroster/Roster.java
@@ -0,0 +1,13 @@
+package com.example.witchbornroster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Roster extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_roster);
+    }
+}
